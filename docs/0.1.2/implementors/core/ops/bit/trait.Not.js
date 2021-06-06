ed9ce7e35cda50146be85e7009cb0980216066e9
Henrik Enquist <henrik.enquist@gmail.com>
2021-06-06 14:21:13 +0200

    Add docs for 0.1.2

---
+++ docs/0.1.2/implementors/core/ops/bit/trait.Not.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["wasapi"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.Not.html\" title=\"trait core::ops::bit::Not\">Not</a> for <a class=\"struct\" href=\"wasapi/Windows/Win32/System/SystemServices/struct.BOOL.html\" title=\"struct wasapi::Windows::Win32::System::SystemServices::BOOL\">BOOL</a>","synthetic":false,"types":["wasapi::Windows::Win32::System::SystemServices::BOOL"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ docs/0.1.2/implementors/core/ops/drop/trait.Drop.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["wasapi"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"wasapi/Windows/Win32/System/OleAutomation/struct.BSTR.html\" title=\"struct wasapi::Windows::Win32::System::OleAutomation::BSTR\">BSTR</a>","synthetic":false,"types":["wasapi::Windows::Win32::System::OleAutomation::BSTR"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
