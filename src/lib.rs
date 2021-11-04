2040e10dc9bded1c2b1a25b1c299ae00773cbef2
Henrik Enquist <henrik.enquist@gmail.com>
2021-11-04 21:18:20 +0100

    Small cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -17,13 +17,14 @@
 //! - Loopback capture
 //! - Notifications for volume change, device disconnect etc
 //!
-//! ## Examples
+//! ## Included examples
 //!
-//! - The `playsine` example plays a sine wave in shared mode on the default output device.
-//!
-//! - The `playsine_events` example is similar to `playsine` but also listens to notifications.
-//!
-//! - The `loopback` example shows how to simultaneously capture and render sound, with separate threads for capture and render.
+//! | Example    | Description                                                    |
+//! | ---------- | ---------------------------------------------------------------|
+//! | `playsine` | Plays a sine wave in shared mode on the default output device. |
+//! | `playsine_events` | Similar to `playsine` but also listens to notifications. |
+//! | `loopback` | Shows how to simultaneously capture and render sound, with separate threads for capture and render. |
+//! | `record` | Records audio from the default device, and saves the raw samples to a file. |
 
 windows_macros::include_bindings!();
 mod api;
